8b8c1ca8e91d38a6a1a782622de409f0e0bda429
Cory Bee <cbinder705@gmail.com>
2023-02-11 21:26:19 -0500

    added constants to index.js for displaying weather data + storing apikey

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-// fetch call to api
+const theCity = document.querySelector("#current-city");
+const theDate = document.querySelector("#current-date");
+const theTemperature = document.querySelector("#temp");
+const theHumidity = document.querySelector("#humidity");
+const theWindSpeed = document.querySelector("#wind-speed");
+const currentSearch = document.getElementById("search-input");
+const searchHistory = localStorage.getItem("#search-history");
+const apiKey = "106f2f72e03bbf98c782d152c4ecb390";
 // query selectors to pass data from api to html
+// fetch call to api
 // date to be formatted using formatDate()
 // create functions to save city searches and to display searches on html
