c3781b3bf8d26b36d09f390c7ad3efb43166fdaf
Cory Bee <cbinder705@gmail.com>
2023-02-11 19:10:30 -0500

    psudo coding to map out application details

---
+++ index.js
@@ -0,0 +1,4 @@
+// fetch call to api
+// query selectors to pass data from api to html
+// date to be formatted using formatDate()
+// create functions to save city searches and to display searches on html
